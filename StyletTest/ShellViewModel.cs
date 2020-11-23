7065990b0da9759745f651b5fc97ceb95008ffe8
Edward Rowe <edward.rowe@bridmet.co.uk>
2020-11-23 12:22:38 +0000

    Add project files.

---
+++ StyletTest/ShellViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StyletTest
+{
+    public class ShellViewModel : Screen
+    {
+        public TaskbarViewModel Taskbar { get; private set; }
+        public ShellViewModel(TaskbarViewModel taskbarViewModel)
+        {
+            this.DisplayName = "Stylet Test Stuff";
+            this.Taskbar = taskbarViewModel;
+        }
+    }
+}
---
+++ StyletTest/TaskbarViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Stylet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace StyletTest
+{
+    public class TaskbarViewModel : Screen
+    {
+        public string TestBinding 
+        { 
+            get { return testBinding; } 
+            set { SetAndNotify(ref testBinding, value); } 
+        }
+        private string testBinding;
+
+        public TaskbarViewModel()
+        {
+            TestBinding = "binding1";
+        }
+
+        public void DoClick(object sender, EventArgs e)
+        {
+            
+            TestBinding = "binding2";
+
+        }
+
+    }
+}
